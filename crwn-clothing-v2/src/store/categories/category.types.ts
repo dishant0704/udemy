6a57c129580bd7f84bb65fb414054c2ac2f1ad4b
Ketan Sawant <ketandutt@gmail.com>
2024-08-31 22:59:05 +0530

    Section 23: Typescript Advanced: Redux Store
    Hi
    Section 23: Typescript Advanced: Redux Store + Middleware
    Done

--- crwn-clothing-v2/src/store/categories/category.types.ts
+++ crwn-clothing-v2/src/store/categories/category.types.ts
@@ -1,22 +1,22 @@
 export enum CATEGORIES_ACTION_TYPES {
-  FETCH_CATEGORIES_START= 'category/FETCH_CATEGORIES_START',
-  FETCH_CATEGORIES_SUCCESS= 'category/FETCH_CATEGORIES_SUCCESS',
-  FETCH_CATEGORIES_FAILED= 'category/FETCH_CATEGORIES_FAILED',
-};
+  FETCH_CATEGORIES_START = 'category/FETCH_CATEGORIES_START',
+  FETCH_CATEGORIES_SUCCESS = 'category/FETCH_CATEGORIES_SUCCESS',
+  FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
+}
 
 export type CategoryItem = {
   id: number;
-  name: string;
   imageUrl: string;
-  price: number;  
-}
+  name: string;
+  price: number;
+};
 
 export type Category = {
-  title:string,
-  imageUrl: string,
-  items: CategoryItem[]
-}
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
 
 export type CategoryMap = {
-  [key:string]: Category[]
-}
+  [key: string]: CategoryItem[];
+};
