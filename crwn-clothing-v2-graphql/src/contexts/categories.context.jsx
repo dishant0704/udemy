8b3b9505a44446188c4fdb2845bc71e9e59eebf6
Ketan Sawant <ketandutt@gmail.com>
2024-09-20 23:38:28 +0530

    section_26_graphQL_apollo_client
    Hi
    Section 26: GraphQL + Apollo Client -- Done
    Thanks
    Ketan

--- crwn-clothing-v2-graphql/src/contexts/categories.context.jsx
+++ crwn-clothing-v2-graphql/src/contexts/categories.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
@@ -6,19 +7,38 @@ export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+ //collections
+ const COLLECTIONS = gql`
+ query{
+ collections{
+   id
+   title
+   items{
+     id
+     name
+     price
+     imageUrl
+   }
+ }
+}
+`
+
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const [categoriesMap, setCategoriesMap] = useState({}); 
+  const { loading, error, data } = useQuery(COLLECTIONS);
+  useEffect(()=>{
+    if(data){
+      const {collections} = data
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const {title, items} = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      },{})
+      console.log("categoriesMap: ", collectionsMap)
+      setCategoriesMap(collectionsMap);
+    }
+  },[data])
+  const value = { categoriesMap,  loading};
   return (
     <CategoriesContext.Provider value={value}>
       {children}
