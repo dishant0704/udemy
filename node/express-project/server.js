ccaa170806fe0de1ec4e67ff1c4fcdfde8ea5889
Ketan Sawant <ketandutt@gmail.com>
2026-07-27 11:44:14 +0530

    Section 08 chapter 86
    Hi
    Code committed for Section 08 chapter 86
    Thank

--- node/express-project/server.js
+++ node/express-project/server.js
@@ -8,8 +8,27 @@ app.get('/', (req, res) => {
   });
 });
 
+const users = [
+  { id: 1, name: 'Ketan' },
+  { id: 2, name: 'John' },
+  { id: 3, name: 'Jane' },
+];
+
 app.get('/message', (req, res) => {
   res.send('Hi ketan!');
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  if(!userId) {
+    return res.status(400).json({ error: 'Invalid user ID' });
+  }else { 
+    return res.json(users.find(user => user.id === userId) || { error: 'User not found' });
+  }
 });
 
 app.post('/message', (req, res) => {
