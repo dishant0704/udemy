a9836b0e94da59d0d164480c125916168d1f28a9
Ketan Sawant <ketandutt@gmail.com>
2026-08-19 11:52:10 +0530

    Section 11 Performance Example --cluster
    Hi
    Code committed for cluster
    Chapter 143 to 150 done.
    Thanks

--- node/performance-example/server.js
+++ node/performance-example/server.js
@@ -1,36 +1,29 @@
-const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
+const express = require("express");
+
 const app = express();
 
-function delay(md){
-    const start = Date.now();
-    while(Date.now() - start < md){
-        // eventloop is blocked
-    };
+function delay(md) {
+  const start = Date.now();
+  while (Date.now() - start < md) {
+    // eventloop is blocked
+  }
 }
 
-app.get('/', (req, res) => {
-    res.send(`Performance Example ${process.pid}`);
-})
+app.get("/", (req, res) => {
+  res.send(`Performance Example ${process.pid}`);
+});
 
-app.get('/timer', (req, res) => {
-    console.log(`/timer started on ${process.pid}`);
-    delay(5000);
-    console.log(`/timer finished on ${process.pid}`);
-    res.send(`eventloop was blocked ${process.pid}`);
-})
+app.get("/timer", (req, res) => {
+  console.log(`/timer started on ${process.pid}`);
+  delay(9000);
+  console.log(`/timer finished on ${process.pid}`);
+  res.send(`Zero downTime Restart by Reload  ${process.pid}`);
+  res.send(`eventloop was blocked ${process.pid}`);
+});
 console.log(`Running server...`);
-if(cluster.isMaster){
-    console.log(`Master ${process.pid} is running`);
-    const numCPUs = os.cpus().length;
-    console.log(`Forking for ${numCPUs} CPUs`);
-    for(let i = 0; i < numCPUs; i++){
-        cluster.fork();
-    }
-}else{
-    console.log(`Worker ${process.pid} started`);
-    app.listen(3000, () => {
-        console.log('server is listening on port 3000');
-    })
-}
+console.log(`Master ${process.pid} is running`);
+console.log(`Worker ${process.pid} started`);
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
