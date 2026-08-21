07c600d00a6daf4f85783cd0572477a2f0fffa27
Ketan Sawant <ketandutt@gmail.com>
2026-08-21 11:50:30 +0530

    Section 12 for Database 151 to 163 done
    Section 12 for Database - mongo-db added.
    Chapter up-to  151 to 163 done.
    Code committed for the same
    Thanks

--- node/nasa-project/server/src/server.js
+++ node/nasa-project/server/src/server.js
@@ -1,15 +1,27 @@
-const http = require('http');
+const http = require("http");
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 8000;
-const app = require('./app');
-const { loadPlanetsData } = require('./models/planets.model');
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
+const MONGODB_URI = "mongodb+srv://ketandutt_db_user:b4CGMFQtvOILldlB@cluster0.6ktl8zl.mongodb.net";
+
+mongoose.connection.once('open', ()=>{
+  console.log('MogoDb Connection is ready!')
+});
+
+mongoose.connection.on('error',(error)=>{
+  console.error(`MogoDb Connection error: ${error}`)
+})
 
 async function startServer() {
-    await loadPlanetsData();
-    server.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+  await mongoose.connect(MONGODB_URI);
+  await loadPlanetsData();
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }
 
 startServer();
