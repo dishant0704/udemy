0f477bbe300cb0b8e538b6a07fe1d04faae76b85
Ketan Sawant <ketandutt@gmail.com>
2026-09-04 06:39:13 +0530

    Section 13 Working With Rest APIS - SpaceX Project
    Hi
    Chapter up-to  190 to 193 done.
    Code committed for the same
    Thanks

--- node/nasa-project/server/src/routes/planetsRoute/planets.test.js
+++ node/nasa-project/server/src/routes/planetsRoute/planets.test.js
@@ -16,7 +16,7 @@ describe("Launch API Test/", () => {
   describe("Test GET planets API", () => {
     test("It should respond with 200 success", async () => {
       const respond = await request(app)
-        .get("/planets")
+        .get("/v1/planets")
         .expect("Content-Type", /json/)
         .expect(200);
     });
